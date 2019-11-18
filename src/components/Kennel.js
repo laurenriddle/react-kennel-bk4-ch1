9bc02e50366e1366d32055d359886c57e782d2bf
Lauren Riddle <lari7132@gmail.com>
2019-11-18 15:59:46 -0600

    removed import statements

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -1,9 +1,5 @@
 import React, { Component } from 'react'
-import AnimalCard from './animal/AnimalCard'
 import './Kennel.css'
-import OwnerCard from './owner/OwnerCard';
-import EmployeeCard from './employee/EmployeeCard';
-import LocationCard from './location/LocationCard';
 import NavBar from './nav/NavBar';
 import ApplicationViews from './ApplicationViews';
 
